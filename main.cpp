c6580d543f06dfe0ba387d2f588cccdabfa19e7e
NicholasHeim <nickheim28@gmail.com>
2020-06-20 20:05:11 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/*
+ *  Author: Nicholas Heim
+ * Contact: nickheim28@gmail.com
+ * Created: June 20, 2020
+ * Purpose: A C++ implementation of the 0-1 Knapsack algorithm using vectors.
+ *          https://en.wikipedia.org/wiki/Knapsack_problem
+ */
